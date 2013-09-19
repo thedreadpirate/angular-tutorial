add5e53c3f59221fa9e390b4c7e017d21fdb40da
Keith <keith@moran-obrien.com>
2013-09-19 09:03:27 -0400

    page 24

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -1,4 +1,13 @@
 window.angular.module('ngff.services.global', [])
   .factory('Global', function() {
-
+  	var current_user = window.user;
+  	return {
+  		current_user: function(){
+  			return current_user;
+  		},
+  		isSignedIn: function(){
+  			return !!current_user;
+  		}
+  	}
+  	
   });
