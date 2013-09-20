f6a0dde7442ecf6f050863e90cce6f764986f260
Keith <keith@moran-obrien.com>
2013-09-19 23:44:49 -0400

    now you can add leagues

--- config/routes.js
+++ config/routes.js
@@ -14,7 +14,15 @@ module.exports = function (app, passport, auth) {
   app.get('/users/:userId', users.show)
   
   app.param('userId', users.user)
-  
+
+  var leagues = require('../app/controllers/leagues')
+  app.get('/leagues', leagues.all)
+  app.post('/leagues', auth.requiresLogin, leagues.create)
+  app.get('/leagues/:leagueId', leagues.show)
+  app.put('/leagues/:leagueId', auth.requiresLogin, leagues.update)
+  app.del('/leagues/:leagueId', auth.requiresLogin, leagues.destroy)
+  app.param('leagueId', leagues.league)
+
   // home route
   var index = require('../app/controllers/index')
   app.get('/', index.render)
